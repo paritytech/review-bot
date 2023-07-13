89c941e6a96087d5ca42d485bcc0b841e8b11ec3
Javier Bullrich <javier@bullrich.dev>
2023-07-13 15:47:39 +0200

    implemented validator in action runner

--- src/runner.ts
+++ src/runner.ts
@@ -1,20 +1,30 @@
 import { parse } from "yaml";
 
 import { Inputs } from ".";
-import { ConfigurationFile } from "./fileHandler";
+import { ConfigurationFile } from "./file/types";
+import { schema } from "./file/validator";
 import { PullRequestApi } from "./github/pullRequest";
 import { ActionLogger } from "./github/types";
 
-/** action in charge of running the GitHub action */
+/** Action in charge of running the GitHub action */
 export class ActionRunner {
   constructor(private readonly prApi: PullRequestApi, private readonly logger: ActionLogger) {}
 
+  /** Fetches the configuration file, parses it and validates it.
+   * If the config is invalid or not found, an error will be thrown.
+   */
   async getConfigFile(configLocation: string): Promise<ConfigurationFile> {
     const content = await this.prApi.getConfigFile(configLocation);
+    this.logger.debug(content);
     const config = parse(content) as ConfigurationFile;
 
-    // TODO: validate the config file
     this.logger.info(`Obtained config at ${configLocation}`);
+
+    const validation = schema.validate(config);
+    if (validation.error) {
+      this.logger.error("Configuration file is invalid");
+      throw validation.error;
+    }
 
     return config;
   }
