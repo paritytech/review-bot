e3fca88249e4b6daaac619a783d6c73819b91ffe
Javier Bullrich <javier.bullrich@parity.io>
2023-07-17 16:28:48 +0200

    added type for basic rule

--- src/file/types.ts
+++ src/file/types.ts
@@ -1,10 +1,31 @@
+enum Rules {
+  Basic = "basic",
+  Debug = "debug",
+}
+
 export interface Rule {
   name: string;
   condition: { include: string[]; exclude?: string[] };
 }
 
+// TODO: Delete this once we add a second type of rule
+export interface DebugRule extends Rule {
+  type: Rules.Debug;
+  size: number;
+}
+
+export interface BasicRule extends Rule {
+  type: Rules.Basic;
+  min_approvals: number;
+  users?: string[];
+  teams?: string[];
+}
+
 export interface ConfigurationFile {
-  rules: Rule[];
+  /** Based on the `type` parameter, Typescript converts the object to the correct type
+   * @see {@link Rules}
+   */
+  rules: (BasicRule | DebugRule)[];
   preventReviewRequests: {
     teams?: string[];
     users: string[];
