39a722746483b7829b018e75c219511ad4fe5a3b
Javier Bullrich <javier.bullrich@parity.io>
2023-08-10 11:58:05 +0200

    created CLI tool

---
+++ src/cli.ts
@@ -0,0 +1,25 @@
+import { readFileSync } from "fs";
+import { parse } from "yaml";
+
+import { ConfigurationFile } from "./file/types";
+import { validateConfig, validateRegularExpressions } from "./file/validator";
+
+const fileLocation = process.argv[2];
+
+if (!fileLocation) {
+  throw new Error("Missing file location! Write the path as a parameter. (More info in the README)");
+}
+
+console.log("Looking for config in", fileLocation);
+const configTxt = readFileSync(fileLocation, "utf8");
+console.log("Found config file");
+const config = parse(configTxt) as ConfigurationFile;
+
+const configFile = validateConfig(config);
+
+const [result, error] = validateRegularExpressions(configFile, console);
+if (!result) {
+  throw new Error(`Regular expression is invalid: ${error}`);
+}
+
+console.log("Config is valid!");
