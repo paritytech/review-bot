882c50b703f1f5af5ecdf3e7ef95c4efa9d59736
Javier Bullrich <javier.bullrich@parity.io>
2023-10-13 15:43:06 +0200

    added Pull Request number as input
    This allows is to fetch the PR directly instead of obtaining it from the event. It is an alternative solution for when we are using a dispatch event.
    
    Resolves #93

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { PullRequest } from "@octokit/webhooks-types";
 import { GitHubChecksApi } from "./github/check";
 import { PullRequestApi } from "./github/pullRequest";
 import { GitHubTeamsApi } from "./github/teams";
+import { CheckData } from "./github/types";
 import { PolkadotFellows } from "./polkadot/fellows";
 import { ActionRunner } from "./runner";
 import { generateCoreLogger } from "./util";
@@ -16,6 +17,8 @@ export interface Inputs {
   repoToken: string;
   /** A custom access token with the read:org access */
   teamApiToken: string;
+  /** Number of the PR to analyze. Optional when it is triggered by `pull_request` event */
+  prNumber: number | null;
 }
 
 const getRepo = (ctx: Context) => {
@@ -36,8 +39,9 @@ const getInputs = (): Inputs => {
   const configLocation = getInput("config-file");
   const repoToken = getInput("repo-token", { required: true });
   const teamApiToken = getInput("team-token", { required: true });
+  const prNumber = getInput("pr-number");
 
-  return { configLocation, repoToken, teamApiToken };
+  return { configLocation, repoToken, teamApiToken, prNumber: prNumber ? parseInt(prNumber) : null };
 };
 
 const repo = getRepo(context);
@@ -50,29 +54,46 @@ if (!context.payload.pull_request) {
 
 debug("Got payload:" + JSON.stringify(context.payload.pull_request));
 
-const inputs = getInputs();
+const runAction = async (): Promise<Pick<CheckData, "conclusion">> => {
+  const inputs = getInputs();
 
-const actionId = `${context.serverUrl}/${repo.owner}/${repo.repo}/actions/runs/${context.runId}`;
+  const actionId = `${context.serverUrl}/${repo.owner}/${repo.repo}/actions/runs/${context.runId}`;
 
-const pr = context.payload.pull_request as PullRequest;
+  const actionInstance = getOctokit(inputs.repoToken);
 
-const api = new PullRequestApi(getOctokit(inputs.repoToken), pr, generateCoreLogger(), actionId);
+  let pr: PullRequest;
+  if (context.payload.pull_request) {
+    pr = context.payload.pull_request as PullRequest;
+  } else if (inputs.prNumber) {
+    debug(`Fetching pull request number #${inputs.prNumber}`);
+    const { data } = await actionInstance.rest.pulls.get({ ...repo, pull_number: inputs.prNumber });
+    pr = data as PullRequest;
+  } else {
+    throw new Error("Payload is not `pull_request` and PR number wasn't provided");
+  }
 
-const logger = generateCoreLogger();
+  const api = new PullRequestApi(actionInstance, pr, generateCoreLogger());
 
-const teamApi = new GitHubTeamsApi(getOctokit(inputs.teamApiToken), repo.owner, logger);
+  const logger = generateCoreLogger();
 
-const checks = new GitHubChecksApi(getOctokit(inputs.teamApiToken), pr, logger, actionId);
+  const teamApi = new GitHubTeamsApi(getOctokit(inputs.teamApiToken), repo.owner, logger);
 
-const fellows = new PolkadotFellows(logger);
+  const checks = new GitHubChecksApi(getOctokit(inputs.teamApiToken), pr, logger, actionId);
 
-const runner = new ActionRunner(api, teamApi, fellows, checks, logger);
+  const fellows = new PolkadotFellows(logger);
 
-runner
-  .runAction(inputs)
+  const runner = new ActionRunner(api, teamApi, fellows, checks, logger);
+
+  const result = await runner.runAction(inputs);
+
+  setOutput("report", JSON.stringify(result));
+
+  return result;
+};
+
+runAction()
   .then((result) => {
     info(`Action run without problem. Evaluation result was '${result.conclusion}'`);
-    setOutput("report", JSON.stringify(result));
   })
   .catch((error) => {
     console.error(error);
