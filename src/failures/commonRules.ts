4c5d351ce75ad3987def2386ce5dea23d0975c95
Javier Bullrich <javier.bullrich@parity.io>
2024-01-24 22:09:35 +0100

    moved failure class into separated classes

---
+++ src/failures/commonRules.ts
@@ -0,0 +1,40 @@
+import { summary } from "@actions/core";
+
+import { toHandle } from "../util";
+import { RequestData, ReviewFailure, RuleSummary } from "./types";
+
+export class DefaultRuleFailure extends ReviewFailure {
+  public readonly usersToRequest: string[];
+  public readonly teamsToRequest: string[];
+
+  constructor(report: RuleSummary & RequestData) {
+    super(report);
+    this.usersToRequest = report.usersToRequest ?? [];
+    this.teamsToRequest = report.teamsToRequest ?? [];
+  }
+
+  generateSummary(): typeof summary {
+    let text = super.generateSummary();
+
+    if (this.usersToRequest.length > 0) {
+      text = text.addHeading("Missing users", 3).addList(this.usersToRequest);
+    }
+    if (this.teamsToRequest.length > 0) {
+      text = text.addHeading("Missing reviews from teams", 3).addList(this.teamsToRequest);
+    }
+
+    if (this.countingReviews.length > 0) {
+      text = text
+        .addHeading("Users approvals that counted towards this rule", 3)
+        .addEOL()
+        .addList(this.countingReviews.map(toHandle))
+        .addEOL();
+    }
+
+    return text;
+  }
+
+  getRequestLogins(): { users: string[]; teams: string[] } {
+    return { users: this.usersToRequest, teams: this.teamsToRequest };
+  }
+}
---
+++ src/failures/fellowsRules.ts
@@ -0,0 +1,44 @@
+import { summary } from "@actions/core";
+
+import { toHandle } from "../util";
+import { ReviewFailure, RuleSummary } from "./types";
+
+export class FellowMissingRankFailure extends ReviewFailure {
+  constructor(
+    ruleInfo: RuleSummary,
+    private readonly missingRank: number,
+  ) {
+    super(ruleInfo);
+  }
+
+  generateSummary(): typeof summary {
+    let text = super.generateSummary();
+
+    text = text
+      .addHeading("Missing reviews from Fellows", 3)
+      .addEOL()
+      .addRaw(`Missing reviews from rank \`${this.missingRank}\` or above`)
+      .addEOL();
+    if (this.missingUsers.length > 0)
+      text = text.addDetails(
+        "GitHub users whose approval counts",
+        `This is a list of all the GitHub users who are rank ${this.missingRank} or above:\n\n - ${this.missingUsers
+          .map(toHandle)
+          .join("\n -")}`,
+      );
+
+    if (this.countingReviews.length > 0) {
+      text = text
+        .addHeading("Users approvals that counted towards this rule", 3)
+        .addEOL()
+        .addList(this.countingReviews.map(toHandle))
+        .addEOL();
+    }
+
+    return text;
+  }
+
+  getRequestLogins(): { users: string[]; teams: string[] } {
+    return { users: [], teams: [] };
+  }
+}
