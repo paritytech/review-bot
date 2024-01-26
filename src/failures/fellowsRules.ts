190031ac17c1f46a4636210c9c2b3e6df1fffcea
Javier Bullrich <javier.bullrich@parity.io>
2024-01-26 12:01:39 +0100

    added error rule to fellow failures

--- src/failures/fellowsRules.ts
+++ src/failures/fellowsRules.ts
@@ -42,3 +42,64 @@ export class FellowMissingRankFailure extends ReviewFailure {
     return { users: [], teams: [] };
   }
 }
+
+export class FellowMissingScoreFailure extends ReviewFailure {
+  public readonly currentScore: number;
+  constructor(
+    ruleInfo: Omit<RuleFailedSummary, "missingUsers" | "countingReviews" | "missingReviews">,
+    public readonly requiredScore: number,
+    approvalsWithScores: [string, number][],
+    missingFellowsWithScore: [string, number][],
+  ) {
+    const unifyFellowWithScore = ([handle, score]: [string, number]) => `${handle} => **${score}**`;
+    super({
+      ...ruleInfo,
+      countingReviews: approvalsWithScores.map(unifyFellowWithScore),
+      missingUsers: missingFellowsWithScore.map(unifyFellowWithScore),
+      missingReviews: 1
+    });
+
+    this.currentScore = approvalsWithScores.reduce((n, [_, score]) => n + score, 0);
+  }
+
+  generateSummary(): typeof summary {
+    let text = summary
+      .emptyBuffer()
+      .addHeading(this.name, 2)
+      .addHeading("Missing minimum required score from Fellows", 4)
+      .addDetails(
+        "Rule explanation",
+        "Rule 'Fellows' gives every fellow a score based on their rank, and required that the sum of all the scores is greater than the required score." +
+        "\n\n" +
+        "For more info found out how the rules work in [Review-bot types](https://github.com/paritytech/review-bot#types)",
+      );
+
+    text = text
+      .addHeading(`Missing a score of ${this.requiredScore}`, 3)
+      .addEOL()
+      .addRaw(`Missing ${this.requiredScore - this.currentScore} in the required score.`)
+      .addEOL()
+      .addRaw(`Current score is ${this.currentScore}/${this.requiredScore}`);
+    if (this.missingUsers.length > 0)
+      text = text.addDetails(
+        "GitHub users whose approval counts",
+        `This is a list of all the Fellows that have not reviewed with their current scores:\n\n - ${this.missingUsers
+          .map(toHandle)
+          .join("\n - ")}`,
+      );
+
+    if (this.countingReviews.length > 0) {
+      text = text
+        .addHeading("Users approvals that counted towards this rule with their scores", 3)
+        .addEOL()
+        .addList(this.countingReviews.map(toHandle))
+        .addEOL();
+    }
+
+    return text;
+  }
+
+  getRequestLogins(): { users: string[]; teams: string[] } {
+    return { users: [], teams: [] };
+  }
+}
